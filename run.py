5cc945a2832ad1afd6b4daf38df59cf9cd181735
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-08-25 00:56:32 +0300

    Run app with configuration variables

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
-from app import app
+# run.py
+
+import os
+
+from app import create_app
+
+config_name = os.getenv('APP_SETTINGS')
+app = create_app(config_name)
+
+print(config_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
