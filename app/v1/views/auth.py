105b08efa9a05ff5442aa32d5e5af433d9e24711
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-09-05 18:33:19 +0300

    [Feature #160249515] Remove flask_expects_json usage

--- app/v1/views/auth.py
+++ app/v1/views/auth.py
@@ -1,42 +1,34 @@
 # app/v1/views/auth
 
-from flask import Blueprint, jsonify, g
+from flask import Blueprint, jsonify, g, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_expects_json import expects_json
 
 from app.v1.models import User
-from app.utilities import check_empty_dict
+from app.utilities import check_empty_dict, check_keys
 
 auth = Blueprint(
     'auth_blueprint', __name__, url_prefix='/stackoverflowlite/api/v1/auth')
-
-schema = {
-    'type': 'object',
-    'properties': {
-            'username': {'type': 'string'},
-            'full-name': {'type': 'string'},
-            'email': {'type': 'string'},
-            'password': {'type': 'string'}
-    },
-    'required': ['email', 'password', 'username']
-}
-
 
 user_instance = User()
 
 
 @auth.route('/signup', methods=['POST'])
-@expects_json(schema)
 def user_signup():
     """Method to register a new user"""
+    user_info = request.get_json()
 
-    user_info = g.data
-    user_info["password"] = generate_password_hash(user_info["password"])
+    # Check if all keys have been provided.
+    if check_keys(user_info, 4):
+        response = jsonify({"Error": "Please provide all fields"})
+        return response, 400
 
     # Check for empty keys before saving
     if check_empty_dict(user_info):
         response = jsonify({"Error": "Fields should not be empty"})
         return response, 400
+
+    user_info["password"] = generate_password_hash(user_info["password"])
 
     # Check if user already exists
     existing_users = user_instance.users
@@ -53,11 +45,16 @@ def user_signup():
 
 
 @auth.route('/signin', methods=['POST'])
-@expects_json(schema)
 def user_login():
     """Method to login a user"""
-    user_info = g.data
+    user_info = request.get_json()
 
+    # Check if all required fields have been provided
+    if check_keys(user_info, 2):
+        response = jsonify({"Error": "Please provide email and password"})
+        return response, 400
+
+        # Check for empty values for keys
     if check_empty_dict(user_info):
         response = jsonify({"Error": "Fields should not be empty"})
         return response, 400
