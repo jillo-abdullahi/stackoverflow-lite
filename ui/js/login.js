d32996e22fb40f5493cf0d9e167d2d8a34178ce1
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-09-19 14:46:29 +0300

    [feat #160604406] consume endpoint for user login

---
+++ ui/js/login.js
@@ -0,0 +1,53 @@
+let login = document.querySelector("#login-form")
+let warning = document.querySelector("#warning")
+
+login.addEventListener('submit', e => {
+    e.preventDefault()
+    let email = document.querySelector("#email").value
+    let password = document.querySelector("#psw").value
+
+    data = { "email": email,
+            "password": password
+        }
+    loginUser(data)
+    
+})
+
+loginUser = (data) => {
+    fetch('http://127.0.0.1:5000/stackoverflowlite/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify(data)
+    })
+    .then((response) => {
+        return handleResponse(response)
+    })
+    .then((res) => {
+        if (res.status == 200) {
+            window.location.href = "dashboard.html"    
+        }
+        })
+    .catch((err) => {
+        if (err.status == 401) {
+            err.json().then(data => {
+                warning.classList.remove('hide')
+                warning.classList.add('show')
+                warning.innerHTML = `Warning! ${data.error}`
+            })
+
+        }
+    }) 
+    
+}
+
+let handleResponse = (response) => {
+    if (!response.ok) {
+        throw response;
+    }
+    else {
+    return response;
+    }
+}
