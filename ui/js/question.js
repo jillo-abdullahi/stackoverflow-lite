4eb8765aa6e0124b57f4ed3bcd19ddeef7535d47
Jillo Abdullahi <jillo.abdulla@gmail.com>
2018-10-01 16:20:54 +0300

    [feat #160162005] add user ability to mark an answer as accepted

--- ui/js/question.js
+++ ui/js/question.js
@@ -1,15 +1,18 @@
 const questionTitle = document.querySelector('#question-title');
 const questionBody = document.querySelector('#question-body');
+const questionAuthored = document.querySelector('#posted-by');
+const questionPostTime = document.querySelector('#posted-at');
 const answerTable = document.querySelector('#questions-table');
 const warning = document.querySelector('#warning');
 const success = document.querySelector('#success');
 const username = document.querySelector('#username-field');
 const loadQuestions = document.querySelector('.load-question');
+const logout = document.querySelector('#logout');
+const postAnswer = document.querySelector('#answersForm');
+const userId = localStorage.getItem('user_id');
+const questionId = localStorage.getItem('questionId');
 const token = localStorage.getItem('token');
 const loggedInAs = localStorage.getItem('username');
-const logout = document.querySelector('#logout');
-const questionId = localStorage.getItem('questionId');
-const postAnswer = document.querySelector('#answersForm');
 
 // Return login message if user is not logged in
 if (localStorage.getItem('token') === null) {
@@ -44,7 +47,10 @@ let getOneQuestion = () => {
                 // Load question title and description onto this page
                 questionTitle.innerHTML = data.question.title;
                 questionBody.innerHTML = data.question.description;
-                
+                questionAuthored.innerHTML = data.question.username;
+                questionPostTime.innerHTML = data.question.date_created;
+
+                const questionAuthor = data.question.user_id;
                 const answers = data.question.Answers;
                 if (answers.length === 0) {
                     warning.classList.remove('hide');
@@ -55,14 +61,52 @@ let getOneQuestion = () => {
                     let output = '';
                     answers.forEach(element => {
                         output += `
-                        <tr>
-                            <td>
-                                <p>${element.title}</p>
-                                <p> - <b>${element.username}</b>
-                                <p>Posted on: ${element.date_created}</P>
-                            </td>
-                        </tr>
-                        `;
+                                <tr>
+                                <td>
+                                    <p>${element.title}</p>
+                                    <p> - <b>${element.username}</b>
+                                    <p>Posted on: ${element.date_created}</p>
+                                    <ul id="buttons-list">
+                                
+                                `;
+                        //Add appropriate buttons based on current user
+                        if (`${questionAuthor}` === `${userId}` && `${element.user_id}` === `${userId}`) {
+                            output += `
+                                <li>
+                                <button onclick="editAnswer(${element.id})" id="edit-qn-btn">
+                                <i class="fa fa-edit"></i> Edit</button>
+                                </li>`;
+                                if (element.preferred !== true) {
+                                    output += `
+                                    <li>
+                                    <button onclick="acceptAnswer(${element.id})" id="accept-ans-btn">
+                                    <i class="fa fa-check"></i> Accept</button>
+                                    </li>
+                                    `;
+                                }       
+                        } else if (`${element.user_id}` === `${userId}`) {
+                            output += `
+                                <li>
+                                <button onclick="editAnswer(${element.id})" id="edit-qn-btn">
+                                <i class="fa fa-edit"></i> Edit</button>
+                                </li>
+                                `;
+                        } else if (`${questionAuthor}` === `${userId}` && element.preferred !== true) {
+                            output += `
+                                <li>
+                                <button onclick="acceptAnswer(${element.id})" id="accept-ans-btn">
+                                <i class="fa fa-check"></i> Accept</button>
+                                </li>
+                                `;
+                        }
+                        if (element.preferred === true) {
+                            output += `
+                            <li><p><span id="accepted">Accepted</span></p></li>`;
+                        }   
+                        output += `
+                                    </ul>
+                                </td>
+                            </tr>`;
                     });
                     answerTable.innerHTML += output;   
                 }
@@ -122,3 +166,34 @@ let userPostAnswer = (details) => {
     });  
 };
 
+// Function to accept answer
+const acceptAnswer = (answerId) => {
+    if (confirm('Please confirm if you would like to accept this answer')) { 
+        fetch(`http://127.0.0.1:5000/stackoverflowlite/api/v1/questions/${questionId}/answers/${answerId}`, {
+        method: 'PUT',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({})
+    })
+    .then((response) => handleResponse(response))
+    .then((res) => {
+        if (res.status === 200) {
+            res.json().then(data => {
+                alert(data.message);
+                const timeOut = () => { location.reload(true); };
+                setTimeout(timeOut, 1000);
+            });     
+        }
+    })
+    .catch((err) => {
+        if (err.status === 403) {
+            err.json().then(data => {
+                alert(data.message);
+            });
+        }
+        });  
+    }  
+};
